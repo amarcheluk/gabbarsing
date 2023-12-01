7ebec0525046532dfb7f0a6dbd215a1c7977cc63
AAdhvikashruthi <shruthigm14@gmail.com>
2023-12-01 16:27:11 +0530

    4th commit

---
+++ src/test/java/Practice3Test.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class Practice3Test {
+	@Test
+	public void test()
+	{
+		System.out.println("hi");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("hello");
+	}
+}
